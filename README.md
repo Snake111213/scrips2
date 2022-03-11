b92cf6ff7ea875e11cfb2c4e58cc603954fbc25c
Snake111213 <99146006+Snake111213@users.noreply.github.com>
2022-03-11 18:05:12 -0400

    Update and rename README.md to tipo.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# calculadora-de-numeros-pares
---
+++ tipo.py
@@ -0,0 +1,7 @@
+# calculadora-de-numeros-pares
+numero = int(input("Ingrese su numero"))
+if numero % 2 == 0:
+    print("el mumero es par")
+
+else:
+    print("el numero es inpar")
